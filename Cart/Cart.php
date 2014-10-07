bad72b85e9e89900f4905032f2d68533f2d88973
Varun Batra <codevarun@gmail.com>
2014-10-07 22:24:02 +0530

    naming consistency

--- Cart/Cart.php
+++ Cart/Cart.php
@@ -105,7 +105,7 @@ class Cart
     /**
      * Delete all types of cart
      */
-    public function resetAllCart()
+    public function removeAllCart()
     {
         $this->storage->set(array());
     }
